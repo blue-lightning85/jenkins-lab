27a7a4b5f8302c4854aa57524a746bb0f992958a
Marcel <marcelvieira424@gmail.com>
2022-05-11 16:07:30 -0400

    fixed unit-test.py

--- unit-test.py
+++ unit-test.py
@@ -3,7 +3,8 @@ import unittest
 
 class Testclass(unittest.TestCase):
   def test_add(x):
-    x.assertEqual(test.add(), 3)
+    x.assertEqual(test.add(), 2)
 
 if __name__ == "__main__":
   unittest.main()
+  
